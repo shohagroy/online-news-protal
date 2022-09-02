7049c35f2c39e807df2f8dfacce857ebf7fbd53d
shohagroy <pkshohag240@gmail.com>
2022-09-03 01:12:51 +0600

    alert section added and update

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 
+// get loading-spiner
+const loadingSpiner = document.querySelector('.loading-spiner');
+loadingSpiner.classList.remove('d-none')
+
 // caragoty api call function 
 fetch('https://openapi.programming-hero.com/api/news/categories')
     .then(res => res.json())
     .then(data => displayCatagory(data.data.news_category))
-
 
     // get category container 
     const mainCategotuDiv = document.querySelector('.category-container');
@@ -11,19 +14,37 @@ fetch('https://openapi.programming-hero.com/api/news/categories')
     // display catagoty function 
     displayCatagory = (categorys)=>{
         categorys.forEach(category => {
-            console.log(category)
             const caragotyDiv = document.createElement('div');
             caragotyDiv.classList.add('p-2')
             caragotyDiv.innerHTML = `
-            <button class="btn btn-primary" onclick="displayNews(${category.category_id})">${category.category_name}</button>
+            <button class="btn btn-primary" onclick="displayNews(${category.category_id}, '${category.category_name}')">${category.category_name}</button>
             `
             mainCategotuDiv.appendChild(caragotyDiv);
         });
+        
+        loadingSpiner.classList.add('d-none')
     }
-
+    
 
 
     // display news 
-    const displayNews = (id)=>{
-        console.log(id)
+    const displayNews = (category_id, caragoty_name)=>{
+    loadingSpiner.classList.remove('d-none')
+
+        console.log(category_id)
+        fetch(`https://openapi.programming-hero.com/api/news/category/0${category_id}`)
+            .then(res => res.json())
+            .then(data => setDisplayNews(data.data))
+
+            setDisplayNews = (news) =>{
+                console.log(news)
+
+                document.querySelector('.alert-container').innerHTML = `
+            <p class="p-2">${news.length} item found for category "${caragoty_name}"</p>`            
+            loadingSpiner.classList.add('d-none')
+        }
+
+
+            
+
     }
